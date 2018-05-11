59c8666eb3e0e78cf9c0a2084a4340d412cc54e5
Stephanie Faber <eyesicedover@gmail.com>
2018-05-11 10:09:15 -0700

    add GitHubAPI model and add API request methods.

--- Demofolio/Models/GitHubAPI.cs
+++ Demofolio/Models/GitHubAPI.cs
@@ -9,10 +9,28 @@ using RestSharp.Authenticators;
 
 namespace Demofolio.Models
 {
-    public class Comment
+    public class GitHubAPI
     {
         public int Id { get; set; }
-		public string RepoName { get; set; }
-		public string RepoUrl { get; set; }
+        public string RepoName { get; set; }
+        public string RepoUrl { get; set; }
     }
+
+    var request = new RestRequest("https://api.github.com/users/jaybojaybojaybo/starred.json", Method.GET);
+
+    var response = new RestResponse();
+
+    Task.Run(async =>
+		{
+			response = await GetResponseContentAsync(request) as RestResponse;
+		}).Wait();
+
+	public static Task<IRestResponse> GetResponseContentAsync(RestRequest theRequest)
+	{
+		var tcs = new TaskCompletionSource<IRestResponse>();
+		theClient.ExecuteAsync(theRequest, response => {
+			tcs.SetResult(response);
+		});
+		return tcs.Task;
+	}
 }
